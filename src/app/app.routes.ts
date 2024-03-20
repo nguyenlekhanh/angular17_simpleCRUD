e4c96eed5367f4d52e7ea7ed47e680b491c59bbd
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-19 21:42:29 -0500

    show employee list in material table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: EmployeeListComponent}
+];
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,17 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { IEmployee } from '../interfaces/employee';
 import { HttpService } from '../http.service';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
   employeeList: IEmployee[] = [];
   httpService = inject(HttpService);
+  displayedColumns: string[] = ['id', 'name', 'email', 'age', 'phone', 'salary'];
 
   ngOnInit() {
     this.httpService.getAllEmployee().subscribe((result) => {
