f8cd1ed5c22acb1137cb93aac8e2b8c4620a941f
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-19 22:15:38 -0500

    create html form for employee form with formsModule and reactiveFormModule

--- src/app/employee-form/employee-form.component.ts
+++ src/app/employee-form/employee-form.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.css'
 })
 export class EmployeeFormComponent {
+  formBuilder = inject(FormBuilder);
 
+  employeeForm = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    age: [0, [Validators.required]],
+    phone: ['', []],
+    salary: [0, [Validators.required]],
+  });
+
+  save() {
+    console.log(this.employeeForm.value);
+  }
 }
